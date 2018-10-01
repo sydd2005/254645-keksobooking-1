e846606a0ac4f5652b26f0d38923bef0dcd480df
Dmitry Spiridonov <sydd2005@gmail.com>
2018-10-01 13:52:09 +0300

    вносит правки по замечаниям к module2-task2

--- src/empty.js
+++ src/empty.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  name: `empty`,
+  name: ``,
   description: `Печатает приветствие с кратким описанием приложения`,
   execute() {
     console.log(`Привет пользователь!
--- src/unknown.js
+++ src/unknown.js
@@ -1,12 +1,8 @@
 'use strict';
 
-const UNKNOWN_COMMAND_MESSAGE = `Неизвестная команда {0}.
-Доступные команды:
---help        — печатает этот текст;
---version     — печатает версию приложения;
---author      — печатает автора приложения;
---license     — печатает тип лицензии;
---description — печатает описание приложения.`;
+const helpCommand = require(`./help`);
+
+const UNKNOWN_COMMAND_MESSAGE = `Неизвестная команда {0}.`;
 
 const interpolateString = (target, ...values) => {
   return target.replace(/\{(\d)\}/g, (match, index) => values[index]);
@@ -17,5 +13,6 @@ module.exports = {
   description: `Печатает список доступных команд с описанием`,
   execute(command) {
     console.error(interpolateString(UNKNOWN_COMMAND_MESSAGE, command));
+    helpCommand.execute();
   },
 };
