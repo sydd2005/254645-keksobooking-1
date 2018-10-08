a027ef13cfc2311ebb091fda4cb9f37c5c595b73
Spiridonov Dmitry <sydd2005@gmail.com>
2018-10-09 01:23:49 +0300

    рефакторит реализацию циклического опроса

--- src/empty.js
+++ src/empty.js
@@ -30,61 +30,97 @@ const question = (query) => {
   });
 };
 
+const questionCyclically = async (cycleCallback, callbackParams) => {
+  let result = {
+    canProceed: false,
+    shouldBreak: false,
+  };
+  while (!result.canProceed && !result.shouldBreak) {
+    result = await cycleCallback(...callbackParams);
+    if (!result || !result.canProceed && result.retryMessage && !result.shouldBreak) {
+      console.log(result.retryMessage);
+    }
+  }
+  return result;
+};
+
+const askForDataGeneration = async () => {
+  const answer = (await question(`Сгенерировать данные? (${AnswerType.POSITIVE}/${AnswerType.NEGATIVE}) `)).toLowerCase();
+  return {
+    answer,
+    canProceed: answer === AnswerType.POSITIVE,
+    shouldBreak: answer === AnswerType.NEGATIVE,
+    retryMessage: `Некорректный ответ: "${answer}", выберите y(да) или n(нет).`,
+  };
+};
+
+const askForElementsAmount = async () => {
+  const answer = await question(`Сколько создать элементов? (${ELEMENTS_COUNT.min}-${ELEMENTS_COUNT.max}) `);
+  const elementsAmount = parseInt(answer, 10);
+  const canProceed = !Number.isNaN(elementsAmount) && elementsAmount >= ELEMENTS_COUNT.min && elementsAmount <= ELEMENTS_COUNT.max;
+  const entities = [];
+  if (canProceed) {
+    for (let i = 0; i < elementsAmount; i++) {
+      entities.push(generateEntity());
+    }
+  }
+  return {
+    answer,
+    data: JSON.stringify(entities),
+    canProceed: !Number.isNaN(elementsAmount) && elementsAmount >= ELEMENTS_COUNT.min && elementsAmount <= ELEMENTS_COUNT.max,
+    shouldBreak: false,
+    retryMessage: `Некорректный ответ: "${answer}", укажите число от ${ELEMENTS_COUNT.min} до ${ELEMENTS_COUNT.max}.`,
+  };
+};
+
+const askForFilePathToSave = async (previousAnswerResult) => {
+  const filePathToSave = await question(`Укажите путь до файла, в котором сохранить данные: `);
+  let canProceed = false;
+  let retryMessage = ``;
+  try {
+    canProceed = await saveFile(filePathToSave, previousAnswerResult.data);
+  } catch (err) {
+    if (err.code === `EEXIST`) {
+      const answer = (await question(`Такой файл уже существует, перезаписать? (${AnswerType.POSITIVE}/${AnswerType.NEGATIVE}) `)).toLowerCase();
+      const shouldOverwrite = answer === AnswerType.POSITIVE;
+      if (shouldOverwrite) {
+        canProceed = await saveFile(filePathToSave, previousAnswerResult.data, true);
+      }
+    } else {
+      retryMessage = `Ошибка при попытке сохранения данных: ${err.message}.`;
+    }
+  }
+
+  return {
+    answer: filePathToSave,
+    data: previousAnswerResult.data,
+    canProceed,
+    shouldBreak: false,
+    retryMessage,
+  };
+};
+
+const saveFile = async (filePathToSave, dataToSave, overwriteIfExists = false) => {
+  const flag = overwriteIfExists ? `w` : `wx`;
+  await writeFile(filePathToSave, dataToSave, {flag});
+  console.log(`Запись данных произведена успешно!`);
+  return true;
+};
+
 const execute = async () => {
   console.log(`Привет, пользователь!`);
 
-  let canProceed = false;
-  while (!canProceed) {
-    const shouldDataBeGenerated = (await question(`Сгенерировать данные? (${AnswerType.POSITIVE}/${AnswerType.NEGATIVE}) `)).toLowerCase();
-    if (shouldDataBeGenerated === AnswerType.NEGATIVE) {
+  const questionHandlers = [
+    askForDataGeneration,
+    askForElementsAmount,
+    askForFilePathToSave,
+  ];
+
+  let previousAnswerResult = {};
+  for (const questionHandler of questionHandlers) {
+    previousAnswerResult = await questionCyclically(questionHandler, [previousAnswerResult]);
+    if (previousAnswerResult.shouldBreak) {
       return rl.close();
-    }
-    canProceed = shouldDataBeGenerated === AnswerType.POSITIVE;
-    if (!canProceed) {
-      console.log(`Некорректный ответ: "${shouldDataBeGenerated}", выберите y(да) или n(нет).`);
-    }
-  }
-
-  canProceed = false;
-  let elementsAmount;
-  while (!canProceed) {
-    const answer = await question(`Сколько создать элементов? (${ELEMENTS_COUNT.min}-${ELEMENTS_COUNT.max}) `);
-    elementsAmount = parseInt(answer, 10);
-    canProceed = !Number.isNaN(elementsAmount) && elementsAmount >= ELEMENTS_COUNT.min && elementsAmount <= ELEMENTS_COUNT.max;
-    if (!canProceed) {
-      console.log(`Некорректный ответ: "${answer}", укажите число от ${ELEMENTS_COUNT.min} до ${ELEMENTS_COUNT.max}.`);
-    }
-  }
-
-  const entities = [];
-  for (let i = 0; i < elementsAmount; i++) {
-    entities.push(generateEntity());
-  }
-  const dataToSave = JSON.stringify(entities);
-
-  canProceed = false;
-  while (!canProceed) {
-    const filePathToSave = await question(`Укажите путь до файла, в котором сохранить данные: `);
-
-    const saveFile = async (overwriteIfExists = false) => {
-      const flag = overwriteIfExists ? `w` : `wx`;
-      await writeFile(filePathToSave, dataToSave, {flag});
-      canProceed = true;
-      console.log(`Запись данных произведена успешно!`);
-    };
-
-    try {
-      await saveFile();
-    } catch (err) {
-      if (err.code === `EEXIST`) {
-        const answer = (await question(`Такой файл уже существует, перезаписать? (${AnswerType.POSITIVE}/${AnswerType.NEGATIVE}) `)).toLowerCase();
-        const shouldOverwrite = answer === AnswerType.POSITIVE;
-        if (shouldOverwrite) {
-          await saveFile(true);
-        }
-      } else {
-        console.error(`Ошибка: ${err.message}.`);
-      }
     }
   }
 
